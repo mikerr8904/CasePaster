1d92d4484bfa8ec1ac8d2e4f2f948320d97637ce
M.Ruiz <59893821+mikerr8904@users.noreply.github.com>
2024-01-20 18:22:52 -0500

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿using System.Windows.Forms;
+
+namespace CasePaster
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.checkBoxClipBoard = new System.Windows.Forms.CheckBox();
+            this.checkBox_eTicket = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "eTicket Case #";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // checkBoxClipBoard
+            // 
+            this.checkBoxClipBoard.AutoSize = true;
+            this.checkBoxClipBoard.Location = new System.Drawing.Point(30, 46);
+            this.checkBoxClipBoard.Name = "checkBoxClipBoard";
+            this.checkBoxClipBoard.Size = new System.Drawing.Size(109, 17);
+            this.checkBoxClipBoard.TabIndex = 0;
+            this.checkBoxClipBoard.Text = "Copy to Clipboard";
+            this.checkBoxClipBoard.UseVisualStyleBackColor = true;
+            // 
+            // checkBox_eTicket
+            // 
+            this.checkBox_eTicket.AutoSize = true;
+            this.checkBox_eTicket.Location = new System.Drawing.Point(30, 68);
+            this.checkBox_eTicket.Name = "checkBox_eTicket";
+            this.checkBox_eTicket.Size = new System.Drawing.Size(96, 17);
+            this.checkBox_eTicket.TabIndex = 1;
+            this.checkBox_eTicket.Text = "Add to eTicket";
+            this.checkBox_eTicket.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(30, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Case # Behavior";
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(121, 87);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(64, 20);
+            this.buttonSave.TabIndex = 6;
+            this.buttonSave.Text = "Save";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(195, 117);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkBox_eTicket);
+            this.Controls.Add(this.checkBoxClipBoard);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "DR# Copy Paste";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private NotifyIcon notifyIcon1;
+        private CheckBox checkBoxClipBoard;
+        private CheckBox checkBox_eTicket;
+        private Label label1;
+        private Button buttonSave;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,291 @@
+﻿using FlaUI.Core.AutomationElements;
+using FlaUI.Core.Tools;
+using FlaUI.UIA2;
+using System.Diagnostics;
+using Gma.System.MouseKeyHook;
+using System.Linq;
+using System.Windows.Forms;
+using System.Windows;
+using System;
+using CasePaster.Properties;
+using Clipboard = System.Windows.Forms.Clipboard;
+using Squirrel;
+using System.Threading.Tasks;
+
+
+
+
+namespace CasePaster
+{
+    public partial class Form1 : Form
+    {
+        private IKeyboardMouseEvents m_GlobalHook;
+
+        public Form1()
+        {
+            InitializeComponent();
+            Subscribe();
+            LoadSettings();
+            CheckFormUpdates();
+        }
+
+        private async Task CheckFormUpdates()
+        {   //TODO update repo addy / config file
+            using (var manager = new UpdateManager(@"githubREPO"))
+            {
+                await manager.UpdateApp();
+            }
+            
+        }
+
+        public void Main_Function()
+        {
+            string caseNumberToPaste = "";
+            string DRbuttonID = "1452";
+            string processSpillman = "Mobile";
+            string processEticket = "CT.ECWS.WinForms";
+            string CaseNumberBoxID = "txtCADNumber";
+            var retrySetting = new RetrySettings()
+            {
+                Timeout = TimeSpan.FromMilliseconds(1500),
+                Interval = TimeSpan.FromMilliseconds(200),
+            };
+
+            using (var automation = new UIA2Automation())
+            {
+                //look for Spillman Mobile Process
+                var process1 = Process.GetProcessesByName(processSpillman).FirstOrDefault();
+
+                if (process1 == null) //Exit and notifiction if not found
+                {
+                    BalloonNotification("Spillman process not found.");
+                    return;
+                }
+
+                using (var appMobile = FlaUI.Core.Application.Attach(process1))
+                {
+                    var windows = Retry.Find(() => appMobile.GetAllTopLevelWindows(automation), retrySetting);
+
+                    if (windows != null)
+                    {
+                        AutomationElement window = windows.FirstOrDefault(w => w.Name.Contains("Flex Mobile"));
+
+                        if (window.IsOffscreen)
+                        {
+                            window.Patterns.Window.Pattern.SetWindowVisualState(FlaUI.Core.Definitions.WindowVisualState.Normal);
+                            window.WaitUntilClickable();
+                        }
+                        window.Focus();
+
+                        //which button to click based on Case # Search setting                        
+                        AutomationElement tabs;
+
+                        var pane = Retry.Find(() => window.FindFirstDescendant(c => c.ByName("CADBar")), retrySetting);
+
+                        var buttonToClick = Retry.Find(() => pane.FindFirstDescendant(cd => cd.ByName("My Call")), retrySetting);
+
+                        if (buttonToClick.IsEnabled == false)
+                        {
+                            BalloonNotification("You do not appear to be on a call", "FAILED");
+
+                            return;
+                        }
+
+                        int j = 0;
+                        do
+                        {
+                            buttonToClick.Click();
+                            j++;
+
+                        } while (j < 3);
+
+                        tabs = Retry.Find(() => window.FindFirstDescendant(cf => cf.ByAutomationId("1120")), retrySetting);
+
+                        var TabToClick = Retry.Find(() => tabs.FindFirstDescendant(cf => cf.ByName("Other")), retrySetting).AsTabItem();
+
+                        TabToClick.WaitUntilClickable(new TimeSpan(500));
+
+                        int i = 0;
+                        do
+                        {
+                            TabToClick.Click();
+                            i++;
+
+                        } while (TabToClick.IsSelected == false && i < 3);
+
+                        if (!TabToClick.IsSelected)
+                        {
+                            BalloonNotification("Could not locate an open call", "FAILED");
+                            return;
+                        }
+
+                        var DrButton = Retry.Find(() => window.FindFirstDescendant(cf => cf.ByAutomationId(DRbuttonID)), retrySetting);
+
+                        if (DrButton != null)
+                        {
+                            BalloonNotification(DrButton.Name);
+                            caseNumberToPaste = DrButton.Name;
+                            if (Settings.Default.Clipboard) { Clipboard.SetText(DrButton.Name); }
+                        }
+                        else
+                        {
+                            BalloonNotification("DR number was not located...", "FAILED");
+                        }                        
+
+                    }
+                    else
+                    {
+                        BalloonNotification("Spillman window not found.");
+                    }
+                }
+
+                //if Add to eTicket is enabled, run this seciton of code
+                if (Settings.Default.eticket)
+                {
+                    var process2 = Process.GetProcessesByName(processEticket).FirstOrDefault();
+
+                    if (process2 == null)
+                    {
+                        BalloonNotification("eTicket process was not found.");
+                        return;
+                    }
+
+                    //look for ticket windown
+                    using (var appTicket = FlaUI.Core.Application.Attach(process2))
+                    {
+                        var DashBoard = Retry.Find(() => appTicket.GetAllTopLevelWindows(automation), retrySetting);
+
+                        if (DashBoard != null && DashBoard[0].AsWindow().ModalWindows.Count() > 0)
+                        {
+                            var Ticket = DashBoard[0].AsWindow().ModalWindows.First(w => w.Name == "Ticket");
+
+                            if (Ticket.IsOffscreen)
+                            {
+                                Ticket.Patterns.Window.Pattern.SetWindowVisualState(FlaUI.Core.Definitions.WindowVisualState.Normal);
+                            }
+
+                            Ticket.Focus();
+
+                            var tabs = Retry.Find(() => Ticket.FindFirstDescendant(cf => cf.ByAutomationId("tabTicket")), retrySetting);
+
+                            var NotesTab = Retry.Find(() => tabs.FindFirstDescendant(cf => cf.ByName(@"Notes/Comments")), retrySetting).AsTabItem();
+
+                            NotesTab.WaitUntilClickable(new TimeSpan(500));
+                            AutomationElement CaseBox;
+                            int i = 0;
+                            do
+                            {
+                                NotesTab.Click();
+                                CaseBox = Retry.Find(() => Ticket.FindFirstDescendant(cf => cf.ByAutomationId(CaseNumberBoxID)), retrySetting);
+                                i++;
+                            } while (CaseBox == null || i < 3);
+
+                            if (CaseBox == null)
+                            {
+                                BalloonNotification("eTicket error", "FAILED");
+                                return;
+                            }
+                            CaseBox.AsTextBox().Text = caseNumberToPaste;
+                        }
+                        else
+                        {
+                            BalloonNotification("Ticket Window Not Located", "FAILED");
+                        }
+
+
+                    }
+
+
+                }
+
+            }
+
+        }
+
+        private void LoadSettings()
+        {
+            this.checkBoxClipBoard.Checked = Settings.Default.Clipboard;
+            this.checkBox_eTicket.Checked = Settings.Default.eticket;
+
+        }
+
+        private void BalloonNotification(string message, string title = "Case Number:")
+        {
+            if (this.notifyIcon1.Visible == false)
+            {
+                this.notifyIcon1.Visible = true;
+            }
+            notifyIcon1.BalloonTipTitle = title;
+            notifyIcon1.BalloonTipText = message;
+            notifyIcon1.BalloonTipIcon = ToolTipIcon.None;
+            notifyIcon1.ShowBalloonTip(1);
+        }
+
+        private void Subscribe()
+        {
+            // Note: for the application hook, use the Hook.AppEvents() instead
+            m_GlobalHook = Hook.GlobalEvents();
+
+            m_GlobalHook.KeyDown += GlobalHookKeyDown;
+        }
+
+        private void GlobalHookKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Modifiers == Keys.Control && e.KeyCode == Keys.F12) // For Ctrl + F12
+            {
+                var s = Settings.Default;
+                if (s.Clipboard | s.eticket)
+                {
+                    Main_Function();
+                }
+                else
+                {
+                    BalloonNotification("No Case # Behavior Setting Saved", "Settings");
+                }
+
+            }
+        }
+
+        private void Unsubscribe()
+        {
+            m_GlobalHook.KeyDown -= GlobalHookKeyDown;
+            m_GlobalHook.Dispose();
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (WindowState == FormWindowState.Minimized)
+            {
+                this.Hide();
+                notifyIcon1.Visible = true;
+            }
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.Show();
+            WindowState = FormWindowState.Normal;
+            notifyIcon1.Visible = false;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Unsubscribe();
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            Settings.Default.Clipboard = this.checkBoxClipBoard.Checked;
+            Settings.Default.eticket = this.checkBox_eTicket.Checked;
+            try
+            {
+                Settings.Default.Save();
+                BalloonNotification("Settings Saved Successfully", "Settings:");
+            }
+            catch (Exception)
+            {
+                BalloonNotification("Settings FAILED to save.");
+            }
+        }
+    }
+}
